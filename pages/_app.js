66ed3455df09008eb75e1dd736c3e6f630f001ab
f1tzy <winndini@gmail.com>
2021-04-26 15:08:20 +0300

    fix errors

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import dynamic from 'next/dynamic'
 import Router from 'next/router';
-import Header from './components/Header/Header';
+import Header from '../components/Header/Header';
 import LazyLoad from 'react-lazyload';
-import Footer from './components/Footer/Footer';
-import NextNProgress from './components/NProgress';
-import NProgress from 'nprogress'
+import Footer from '../components/Footer/Footer';
 import {useEffect, useState} from 'react';
 
 function MyApp({Component, pageProps}) {
