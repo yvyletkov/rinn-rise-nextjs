279262aabbb65e33ec585ca38131b6846f77df9c
Yaroslav Vyletkov <yvyletkov@ya.ru>
2021-04-26 17:38:13 +0300

    add preloader

---
+++ components/Loader/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './styles.module.scss'
+
+export default function Loader() {
+    return (
+        <div className={s.wrapper}>
+            <div className={s.ldsRoller}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
