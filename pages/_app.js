178237282cd71baf209c9a59a71de14ad0b8be42
Yaroslav Vyletkov <yvyletkov@ya.ru>
2021-04-26 14:24:29 +0300

    sync

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import '../styles/globals.scss';
-import '../styles/fonts.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Router from 'next/router';
-import Header from './components/Header/Header';
-import LazyLoad from 'react-lazyload';
-import Footer from './components/Footer/Footer';
+import '../styles/globals.scss'
+import '../styles/fonts.css'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Header from '../components/Header/Header'
+import LazyLoad from 'react-lazyload'
+import Footer from '../components/Footer/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
