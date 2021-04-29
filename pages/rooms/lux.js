f515dc69d8381a97825a641e19b0ec9017dca73d
f1tzy <winndini@gmail.com>
2021-04-29 14:08:42 +0300

    add description room block

--- pages/rooms/lux.js
+++ pages/rooms/lux.js
@@ -2,18 +2,26 @@ import Head from 'next/head'
 import React from 'react'
 import SingleRoomPageBanner from '../../components/pageBanners/SingleRoomPageBanner'
 import DescriptionRoomBlock from '../../components/shared/DescriptionRoomBlock';
-import ServicesSlider from '../../components/sliders/ServicesSlider';
+
+
+const icons = [
+    {img: '/images/rooms/area.svg', text: 'Площадь<br>50м&sup2;'},
+    {img: '/images/rooms/capacity.svg', text: '2 человка<br>1 доп. место'},
+    {img: '/images/rooms/room.svg', text: 'Две<br>комнаты'},
+]
+
 
 export default function SingleRoomPage() {
     return (<>
-        <Head>
-            <title>Rinn Rise – Номер категории Люкс – Анапа, Джемете</title>
-        </Head>
-        <main>
-            <SingleRoomPageBanner />
-            <section className='section first'>
-                <DescriptionRoomBlock title='Описание номера'/>
-            </section>
-        </main>
+            <Head>
+                <title>Rinn Rise – Номер категории Люкс – Анапа, Джемете</title>
+            </Head>
+            <main>
+                <SingleRoomPageBanner/>
+                <section className="section first">
+                    <DescriptionRoomBlock title="Описание номера" icons={icons}/>
+                </section>
+            </main>
         </>
-    )}
+    )
+}
