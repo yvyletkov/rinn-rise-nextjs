bb6a0fc45d0c4379d5803db265ea7dcb2a9058cf
f1tzy <winndini@gmail.com>
2021-04-29 14:57:27 +0300

    add slider

--- pages/rooms/lux.js
+++ pages/rooms/lux.js
@@ -2,13 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import SingleRoomPageBanner from '../../components/pageBanners/SingleRoomPageBanner'
 import DescriptionRoomBlock from '../../components/shared/DescriptionRoomBlock'
-import ServicesSlider from '../../components/sliders/ServicesSlider'
-
-const icons = [
-    {img: '/images/rooms/area.svg', text: 'Площадь<br>50м&sup2;'},
-    {img: '/images/rooms/capacity.svg', text: '2 человка<br>1 доп. место'},
-    {img: '/images/rooms/room.svg', text: 'Две<br>комнаты'},
-]
+import {icons, otherRoomsSlides} from '../../data/room-data';
 
 export default function SingleRoomPage() {
     return (<>
